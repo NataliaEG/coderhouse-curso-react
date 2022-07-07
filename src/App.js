98ca0405b11a534d16e6dc8d74ac5af2186ab1a1
natalia <natalia.gomez@innew.la>
2022-07-06 23:03:43 -0300

    Mi primer commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Holis.
         </p>
         <a
           className="App-link"
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Clase 1 de React JS
         </a>
       </header>
     </div>
