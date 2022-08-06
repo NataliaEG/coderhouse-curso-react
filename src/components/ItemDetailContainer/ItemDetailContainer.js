c9d20f87dbaf6c7bc2ef5b8cbcba1a5e83c1edbb
natalia gomez <nataliagomez2808@gmail.com>
2022-08-06 20:21:23 -0300

    sincro counter

--- src/components/ItemDetailContainer/ItemDetailContainer.js
+++ src/components/ItemDetailContainer/ItemDetailContainer.js
@@ -3,11 +3,12 @@ import { getProd } from '../../asyncMock';
 import { useState,useEffect } from 'react';
 import ItemCount from '../ItemCount/ItemCount';
 import {useParams} from 'react-router-dom';
-
+import {Link} from 'react-router-dom';
 
 const ItemDetailContainer = (props) => { 
 
     const [product, setProduct] = useState([])
+    const [quantity, setQuantity] = useState(0)
 
     const {productId} = useParams()
 
@@ -21,6 +22,7 @@ const ItemDetailContainer = (props) => {
 
     const handleOnAdd = (quantity) => {
         console.log('Cantidad de items agregados', quantity)
+        setQuantity(quantity)
     }
         
 
@@ -37,7 +39,7 @@ const ItemDetailContainer = (props) => {
                     <div className='rightProduct'>
                         <p key={product.id} className='nameDetailsProduct'>{product.name}</p>
                         <p className='priceDetailsProduct'>${product.price}</p>    
-                        <ItemCount stock ={10} initial={1} onAdd={handleOnAdd} />  
+                        {quantity > 0 ? <Link to="/cart" className='link-cart'>Ir al carrito</Link> : <ItemCount stock ={10} initial={1} onAdd={handleOnAdd} />  }
                     </div>
                     
                 </div>
